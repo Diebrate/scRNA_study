f817e2c96486ec35883a15663a3d67a15d63a4b5
Diebrate <kevinkw.zhang@mail.utoronto.ca>
2023-09-06 02:42:03 -0400

    update

--- test/wot_real_data.py
+++ test/wot_real_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.transforms import Bbox
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import os
@@ -93,7 +94,7 @@ x = list(range(T - 1))
 # z = np.polyfit(x, [float(i) for i in cost], 7)
 # f = np.poly1d(z)
 
-graph_cost0 = True
+graph_cost0 = False
 
 if graph_cost0:
 
@@ -142,7 +143,86 @@ if graph_tmap:
         reform_time_name = time_names[i].replace('.', '_') + 'to' + time_names[i+1].replace('.', '_')
         # plt.savefig(r'..\image\wot_tmap\transport_map_'+reform_time_name)
 
-graph_all = True
+graph_tmap_hist = True
+
+from matplotlib.transforms import Bbox
+
+if graph_tmap_hist:
+    for i in range(T - 1):
+        fig, axs = plt.subplots(2, 2, sharex="col", sharey="row", figsize=(8, 8),
+                                gridspec_kw=dict(height_ratios=[1, 2.5], width_ratios=[2.5, 1],
+                                                 hspace=0.01, wspace=0.02))
+        axs[0, 1].set_visible(False)
+        axs[0, 0].set_box_aspect(1/2.5)
+        axs[1, 1].set_box_aspect(2.5/1)
+
+        # Plot the heatmap
+        im = axs[1, 0].imshow(tmap[i], 'viridis')
+
+        # Create colorbar with label at the bottom
+        cbar = fig.colorbar(im, ax=axs[1, 0], location="bottom")
+        cbar.ax.set_xlabel('Probability', rotation=0, ha="center")
+
+        # Adjust colorbar position and size
+        cbar.ax.set_position([0.08, 0.15, 0.65, 0.02])
+
+        # Show all ticks and label them with the respective list entries.
+        axs[1, 0].set_xticks(np.arange(k), labels=type_list)
+        axs[1, 0].set_yticks(np.arange(k), labels=type_list)
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(axs[1, 0].get_xticklabels(), rotation=-30, ha="right", rotation_mode="anchor")
+
+        # Turn spines off and remove gridlines for the heatmap
+        axs[1, 0].spines[:].set_visible(False)
+        axs[1, 0].xaxis.grid(False)
+        axs[1, 0].yaxis.grid(False)
+
+        axs[1, 0].set_xticks(np.arange(k + 1) -.5, minor=True)
+        axs[1, 0].set_yticks(np.arange(k + 1) -.5, minor=True)
+        axs[1, 0].grid(which="minor", color="w", linestyle='-', linewidth=3)
+        axs[1, 0].tick_params(which="minor", bottom=False, left=False)
+
+        plt.setp(axs[1,0].get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+        # Remove gridlines for histograms
+        axs[1, 1].xaxis.grid(False)
+        axs[1, 1].yaxis.grid(False)
+        axs[0, 0].xaxis.grid(False)
+        axs[0, 0].yaxis.grid(False)
+
+        # Rotate the tick labels and set their alignment for histograms
+        plt.setp(axs[1, 1].get_xticklabels(), rotation=0, ha="center")
+        plt.setp(axs[0, 0].get_xticklabels(), rotation=0, ha="center")
+
+        # Rotate the tick labels and set their alignment for row histogram
+        axs[1, 1].set_yticklabels(type_list, rotation=0, ha="center")
+        axs[1, 1].set_xticks(axs[1, 1].get_xticks(), minor=False)
+
+        # Rotate the tick labels and set their alignment for column histogram
+        axs[0, 0].set_xticklabels(type_list, rotation=45, ha="right", minor=False)
+
+        # Plot the histograms
+        axs[1, 1].barh(y=type_list, width=tmap[i].sum(axis=1))
+        axs[0, 0].bar(x=type_list, height=tmap[i].sum(axis=0))
+
+        # Resize
+        (x0m, y0m), (x1m, y1m) = axs[1, 0].get_position().get_points()  # main heatmap
+        (x0h, y0h), (x1h, y1h) = axs[0, 0].get_position().get_points()  # horizontal histogram
+        axs[0, 0].set_position(Bbox([[x0m, y0h], [x1m, y1h]]))
+        (x0v, y0v), (x1v, y1v) = axs[1, 1].get_position().get_points()  # vertical histogram
+        axs[1, 1].set_position(Bbox([[x0v, y0m], [x1v, y1m]]))
+
+        # Set separate titles for the histograms
+        axs[1, 1].set_title('Cell type distribution at ' + time_names[i])
+        axs[0, 0].set_title('Cell type distribution at ' + time_names[i+1])
+
+        fig.suptitle('Transport map from ' + time_names[i] + ' to ' + time_names[i+1])
+
+        reform_time_name = time_names[i].replace('.', '_') + 'to' + time_names[i+1].replace('.', '_')
+        # plt.savefig(r'..\image\wot_tmap\transport_map_hist_'+reform_time_name)
+
+graph_all = False
 
 if graph_all:
     fig, axs = plt.subplots(9, 4, figsize=(36, 85))
@@ -162,5 +242,56 @@ if graph_all:
     # plt.savefig(r'../image/tmap_all.png')
 
 print(cp_days)
+
+# EDA
+
+do_EDA = False
+if do_EDA:
+
+    plt.figure(figsize=(10, 6))
+    ax = plt.subplot(111)
+
+    # Plot each category as a line
+    for category in range(k):
+        ax.plot(range(T), probs[:, category], label=type_list[category])
+
+    # Add labels and legend
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Probability')
+    ax.set_title('Evolution of Cell Type Distribution over Time')
+
+    # Set the x-axis ticks and labels
+    ax.set_xticks(range(T))
+    ax.set_xticklabels(time_names, rotation=70, size=10)
+
+    ax.legend()
+
+    # Adjust the layout to prevent label cutoff
+    plt.tight_layout()
+
+    # histogram
+
+    # Calculate the number of rows and columns for the subplot grid
+    rows = int(np.ceil(np.sqrt(T)))
+    cols = int(np.ceil(T / rows))
+
+    # Create a figure and a grid of subplots
+    fig, axs = plt.subplots(rows, cols, figsize=(12, 8))
+
+    # Flatten the axs array for easy indexing
+    axs = axs.flatten()
+
+    # Create histograms for each row of probs
+    for i in range(T):
+        ax = axs[i]
+        ax.hist(probs[i], bins=20, range=(0, 1), alpha=0.5)
+        ax.set_title(time_names[i])
+        ax.set_xlabel('Cell Type')
+        ax.set_ylabel('Probability')
+
+    # Hide any remaining empty subplots
+    for i in range(T, rows * cols):
+        axs[i].axis('off')
+    plt.tight_layout()
 
 print("--- %s seconds ---" % (time.time() - start_time))
